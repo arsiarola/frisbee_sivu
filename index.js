9c0648140e7b000aac7e3b7bcfc83174b07cef81
jooname <joona.eskonpekka@metropolia.fi>
2019-03-07 11:24:44 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,92 @@
+import Feature from 'ol/Feature.js';
+import Map from 'ol/Map.js';
+import Overlay from 'ol/Overlay.js';
+import View from 'ol/View.js';
+import Point from 'ol/geom/Point.js';
+import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer.js';
+import TileJSON from 'ol/source/TileJSON.js';
+import VectorSource from 'ol/source/Vector.js';
+import {Icon, Style} from 'ol/style.js';
+
+var iconFeature = new Feature({
+    geometry: new Point([0, 0]),
+    name: 'Tämä on popup',
+    population: 4000,
+    rainfall: 500
+});
+
+var iconStyle = new Style({
+    image: new Icon(/** @type {module:ol/style/Icon~Options} */ ({
+        anchor: [0.5, 46],
+        anchorXUnits: 'fraction',
+        anchorYUnits: 'pixels',
+        src: 'https://upload.wikimedia.org/wikipedia/commons/7/78/Maurice_Ravel_1925.jpg'
+    }))
+});
+
+iconFeature.setStyle(iconStyle);
+
+var vectorSource = new VectorSource({
+    features: [iconFeature]
+});
+
+var vectorLayer = new VectorLayer({
+    source: vectorSource
+});
+
+var rasterLayer = new TileLayer({
+    source: new TileJSON({
+        url: 'https://api.tiles.mapbox.com/v3/mapbox.geography-class.json?secure',
+        crossOrigin: ''
+    })
+});
+
+var map = new Map({
+    layers: [rasterLayer, vectorLayer],
+    target: document.getElementById('map'),
+    view: new View({
+        center: [0, 0],
+        zoom: 3
+    })
+});
+
+var element = document.getElementById('popup');
+
+var popup = new Overlay({
+    element: element,
+    positioning: 'bottom-center',
+    stopEvent: false,
+    offset: [0, -50]
+});
+map.addOverlay(popup);
+
+// display popup on click
+map.on('click', function(evt) {
+    var feature = map.forEachFeatureAtPixel(evt.pixel,
+        function(feature) {
+            return feature;
+        });
+    if (feature) {
+        var coordinates = feature.getGeometry().getCoordinates();
+        popup.setPosition(coordinates);
+        $(element).popover({
+            placement: 'top',
+            html: true,
+            content: feature.get('name')
+        });
+        $(element).popover('show');
+    } else {
+        $(element).popover('destroy');
+    }
+});
+
+// change mouse cursor when over marker
+map.on('pointermove', function(e) {
+    if (e.dragging) {
+        $(element).popover('destroy');
+        return;
+    }
+    var pixel = map.getEventPixel(e.originalEvent);
+    var hit = map.hasFeatureAtPixel(pixel);
+    map.getTarget().style.cursor = hit ? 'pointer' : '';
+});
