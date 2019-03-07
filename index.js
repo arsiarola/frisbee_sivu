2851966e4a01fbd11f7114352779de02f4915a51
arsia <arsia@metropolia.fi>
2019-03-07 17:46:00 +0200

    joo vähä heitettii kamaa functioihi, jsonnii, jsonnii

--- index.js
+++ index.js
@@ -11,32 +11,71 @@ import VectorSource from "ol/source/Vector";
 import VectorLayer from "ol/layer/Vector";
 import {fromLonLat} from "ol/proj";
 
-var iconFeature = new Feature({
-    geometry: new Point(fromLonLat([24.945831, 60.192059])),
-    name: 'dasadfgeththethte',
-    population: 4000,
-    rainfall: 500
-});
+function makeMarker(lat, lon, name) {
+    var iconFeature = new Feature({
+        geometry: new Point(fromLonLat([lon, lat])),
+        name: name,
+        population: 4000,
+        rainfall: 500
+    });
 
-var iconStyle = new Style({
-    image: new Icon(/** @type {module:ol/style/Icon~Options} */ ({
-        anchor: [0.5, 500],
-        anchorXUnits: 'fraction',
-        anchorYUnits: 'pixels',
-        src: 'data/mark.png',
-        scale: 0.1
-    }))
-});
+    var iconStyle = new Style({
+        image: new Icon(/** @type {module:ol/style/Icon~Options} */ ({
+            anchor: [0.5, 500],
+            anchorXUnits: 'fraction',
+            anchorYUnits: 'pixels',
+            src: 'data/mark.png',
+            scale: 0.1
+        }))
+    });
 
-iconFeature.setStyle(iconStyle);
+    iconFeature.setStyle(iconStyle);
 
-var vectorSource = new VectorSource({
-    features: [iconFeature]
-});
+    return iconFeature;
+}
 
-var vectorLayer = new VectorLayer({
-    source: vectorSource
-});
+function makeVectorLayer (markerArray) {
+    var vectorSource = new VectorSource({
+        features: markerArray
+    });
+
+    var vectorLayer = new VectorLayer({
+        source: vectorSource
+    });
+    return vectorLayer;
+}
+var markerArray = [];
+markerArray.push(makeMarker(60.192059, 24.945831,  'kenttä: helsinki' +'\n'+ 'sää: pekka pouta'));
+markerArray.push(makeMarker(50.192059, 24.945831,  'helsinki'));
+markerArray.push(makeMarker(40.192059, 24.945831,  'helsinki'));
+var vectorLayer = makeVectorLayer(markerArray);
+
+// var iconFeature = new Feature({
+//     geometry: new Point(fromLonLat([24.945831, 60.192059])),
+//     name: 'dasadfgeththethte',
+//     population: 4000,
+//     rainfall: 500
+// });
+//
+// var iconStyle = new Style({
+//     image: new Icon(/** @type {module:ol/style/Icon~Options} */ ({
+//         anchor: [0.5, 500],
+//         anchorXUnits: 'fraction',
+//         anchorYUnits: 'pixels',
+//         src: 'data/mark.png',
+//         scale: 0.1
+//     }))
+// });
+//
+// iconFeature.setStyle(iconStyle);
+
+// var vectorSource = new VectorSource({
+//     features: [iconFeature]
+// });
+//
+// var vectorLayer = new VectorLayer({
+//     source: vectorSource
+// });
 
 const map = new Map({
     target: document.getElementById('map'),
@@ -47,7 +86,7 @@ const map = new Map({
     ],
     view: new View({
         center: [0,0],
-        zoom: 5
+        zoom: 3
     })
 });
 
