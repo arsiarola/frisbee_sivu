48efaddf94f105e61ceec4ffa680b0921b24e49b
arsia <arsia@metropolia.fi>
2019-03-11 16:43:54 +0200

    Merge branch 'master' of https://gitlab.metropolia.fi/jooname/frisbee_sivu
    # Conflicts:
    #	dist/frisbee_sivu.e31bb0bc.js
    #	dist/galleria.8e9f8092.js
    #	dist/galleria.8e9f8092.map
    #	dist/galleria.c48ba936.css
    #	dist/galleria.c48ba936.js
    #	dist/popup.c6578e23.js
    #	node_modules/data/package.json
    #	node_modules/underscore/package.json

--- dist/frisbee_sivu.e31bb0bc.js
+++ dist/frisbee_sivu.e31bb0bc.js
@@ -49018,7 +49018,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52210" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52983" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
